760fe68a1fe8332774ddd61dd9b37c9ae283a1d5
urie8 <dionpeci08@gmail.com>
2024-03-04 13:28:48 +0100

    Adminrole and user fix

---
+++ ValhallaVaultCyberAwareness/ValhallaVaultCyberAwareness/Managers/AdminManager.cs
@@ -0,0 +1,29 @@
+﻿using ValhallaVaultCyberAwareness.Data;
+
+namespace ValhallaVaultCyberAwareness.Managers
+{
+    public class AdminManager
+    {
+        ApplicationDbContext _context;
+
+        public AdminManager(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public void AddCategory()
+        {
+
+        }
+
+        public void UpdateCategory()
+        {
+
+        }
+
+        public void DeleteCategory()
+        {
+
+        }
+    }
+}
