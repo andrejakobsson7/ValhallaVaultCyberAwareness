a9dacf316425ab294ce94f87e570635c5f940778
andrejakobsson7 <andre.jakobsson7@hotmail.com>
2024-03-10 10:24:40 +0100

    Add scoped services to client project

--- ValhallaVaultCyberAwareness/ValhallaVaultCyberAwareness.Client/Program.cs
+++ ValhallaVaultCyberAwareness/ValhallaVaultCyberAwareness.Client/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using ValhallaVaultCyberAwareness.Client;
+using ValhallaVaultCyberAwareness.Client.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
@@ -8,6 +9,13 @@ builder.Services.AddScoped(http => new HttpClient
 {
     BaseAddress = new Uri("https://localhost:7107/"),
 });
+
+builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddScoped<ISegmentService, SegmentService>();
+builder.Services.AddScoped<ISubCategoryService, SubCategoryService>();
+builder.Services.AddScoped<IQuestionService, QuestionService>();
+builder.Services.AddScoped<IAnswerService, AnswerService>();
+builder.Services.AddScoped<IUserAnswersService, UserAnswersService>();
 
 builder.Services.AddAuthorizationCore();
 builder.Services.AddCascadingAuthenticationState();
