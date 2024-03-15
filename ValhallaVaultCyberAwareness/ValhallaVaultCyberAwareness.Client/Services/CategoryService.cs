83c27bcc4681bc7894eb8b69a0fca12ad828d3c7
andrejakobsson7 <andre.jakobsson7@hotmail.com>
2024-03-15 12:41:58 +0100

    Complete pages for home and category

--- ValhallaVaultCyberAwareness/ValhallaVaultCyberAwareness.Client/Services/CategoryService.cs
+++ ValhallaVaultCyberAwareness/ValhallaVaultCyberAwareness.Client/Services/CategoryService.cs
@@ -5,98 +5,119 @@ using ValhallaVaultCyberAwareness.Domain.Models;
 
 namespace ValhallaVaultCyberAwareness.Client.Services
 {
-    public class CategoryService : ICategoryService
-    {
-        public HttpClient Client { get; set; }
-        public CategoryService(HttpClient client)
-        {
-            Client = client;
-        }
-        public async Task<List<CategoryModel>> GetAllCategoriesAsync()
-        {
-            var apiResponse = await Client.GetAsync("api/category/");
-            if (apiResponse.IsSuccessStatusCode)
-            {
-                string jsonCategories = await apiResponse.Content.ReadAsStringAsync();
-                List<CategoryModel> allCategories = JsonConvert.DeserializeObject<List<CategoryModel>>(jsonCategories);
-                if (allCategories == null)
-                {
-                    throw new JsonException();
-                }
-                else
-                {
-                    return allCategories;
-                }
-            }
-            throw new HttpRequestException();
-        }
+	public class CategoryService : ICategoryService
+	{
+		public HttpClient Client { get; set; }
+		public CategoryService(HttpClient client)
+		{
+			Client = client;
+		}
+		public async Task<List<CategoryModel>> GetAllCategoriesAsync()
+		{
+			var apiResponse = await Client.GetAsync("api/category/");
+			if (apiResponse.IsSuccessStatusCode)
+			{
+				string jsonCategories = await apiResponse.Content.ReadAsStringAsync();
+				List<CategoryModel> allCategories = JsonConvert.DeserializeObject<List<CategoryModel>>(jsonCategories);
+				if (allCategories == null)
+				{
+					throw new JsonException();
+				}
+				else
+				{
+					return allCategories;
+				}
+			}
+			throw new HttpRequestException();
+		}
 
-        public async Task<List<CategoryScoreViewModel>> GetAllCategoriesWithUserScores(string userId)
-        {
-            var apiResponse = await Client.GetAsync($"api/category/{userId}");
-            if (apiResponse.IsSuccessStatusCode)
-            {
-                string jsonCategories = await apiResponse.Content.ReadAsStringAsync();
-                List<CategoryModel>? categories = JsonConvert.DeserializeObject<List<CategoryModel>>(jsonCategories);
-                if (categories == null)
-                {
-                    throw new JsonException();
-                }
-                else
-                {
-                    List<CategoryScoreViewModel> categoryScores = categories.Select(c => new CategoryScoreViewModel(c)).ToList();
-                    return categoryScores;
-                }
-            }
-            throw new HttpRequestException();
-        }
+		public async Task<List<CategoryScoreViewModel>> GetAllCategoriesWithUserScores(string userId)
+		{
+			var apiResponse = await Client.GetAsync($"api/category/{userId}");
+			if (apiResponse.IsSuccessStatusCode)
+			{
+				string jsonCategories = await apiResponse.Content.ReadAsStringAsync();
+				List<CategoryModel>? categories = JsonConvert.DeserializeObject<List<CategoryModel>>(jsonCategories);
+				if (categories == null)
+				{
+					throw new JsonException();
+				}
+				else
+				{
+					List<CategoryScoreViewModel> categoryScores = categories.Select(c => new CategoryScoreViewModel(c)).ToList();
+					return categoryScores;
+				}
+			}
+			throw new HttpRequestException();
+		}
 
-        public async Task<CategoryModel> GetCategoryByIdAsync(int categoryId)
-        {
-            var apiResponse = await Client.GetAsync($"/api/category/{categoryId}/");
-            if (apiResponse.IsSuccessStatusCode)
-            {
-                string jsonCategory = await apiResponse.Content.ReadAsStringAsync();
-                CategoryModel category = JsonConvert.DeserializeObject<CategoryModel>(jsonCategory);
-                if (category == null)
-                {
-                    throw new JsonException();
-                }
-                else
-                {
-                    return category;
-                }
-            }
-            throw new HttpRequestException();
-        }
+		public async Task<CategoryScoreViewModel> GetCategoryWithUserScore(int categoryId, string userId)
+		{
+			var apiResponse = await Client.GetAsync($"api/category/{categoryId}/{userId}");
+			if (apiResponse.IsSuccessStatusCode)
+			{
+				string jsonCategory = await apiResponse.Content.ReadAsStringAsync();
+				CategoryModel? category = JsonConvert.DeserializeObject<CategoryModel>(jsonCategory);
+				if (category == null)
+				{
+					throw new JsonException();
+				}
+				else
+				{
+					CategoryScoreViewModel categoryScore = new CategoryScoreViewModel(category);
+					return categoryScore;
+				}
+			}
+			throw new HttpRequestException();
 
-        public async Task<bool> AddCategoryAsync(CategoryModel newCategory)
-        {
-            var apiResponse = await Client.PostAsJsonAsync("/api/category/", newCategory);
-            if (apiResponse.IsSuccessStatusCode)
-            {
-                return true;
-            }
-            return false;
-        }
+		}
 
-        public async Task<bool> RemoveCategoryAsync(int categoryId)
-        {
-            var apiResponse = await Client.DeleteAsync($"/api/Category/{categoryId}/");
-            if (apiResponse.IsSuccessStatusCode)
-            {
-                return true;
-            }
-            return false;
-        }
-        public async Task<bool> UpdateCategoryAsync(CategoryModel category)
-        {
-            var apiResponse = await Client.PutAsJsonAsync($"/api/Category/{category.Id}", category);
-            if (apiResponse.IsSuccessStatusCode)
-            {
-                return true;
-            }
-            return false;
-        }
-    }
+		public async Task<CategoryModel> GetCategoryByIdAsync(int categoryId)
+		{
+			var apiResponse = await Client.GetAsync($"/api/category/{categoryId}/");
+			if (apiResponse.IsSuccessStatusCode)
+			{
+				string jsonCategory = await apiResponse.Content.ReadAsStringAsync();
+				CategoryModel category = JsonConvert.DeserializeObject<CategoryModel>(jsonCategory);
+				if (category == null)
+				{
+					throw new JsonException();
+				}
+				else
+				{
+					return category;
+				}
+			}
+			throw new HttpRequestException();
+		}
+
+		public async Task<bool> AddCategoryAsync(CategoryModel newCategory)
+		{
+			var apiResponse = await Client.PostAsJsonAsync("/api/category/", newCategory);
+			if (apiResponse.IsSuccessStatusCode)
+			{
+				return true;
+			}
+			return false;
+		}
+
+		public async Task<bool> RemoveCategoryAsync(int categoryId)
+		{
+			var apiResponse = await Client.DeleteAsync($"/api/Category/{categoryId}/");
+			if (apiResponse.IsSuccessStatusCode)
+			{
+				return true;
+			}
+			return false;
+		}
+		public async Task<bool> UpdateCategoryAsync(CategoryModel category)
+		{
+			var apiResponse = await Client.PutAsJsonAsync($"/api/Category/{category.Id}", category);
+			if (apiResponse.IsSuccessStatusCode)
+			{
+				return true;
+			}
+			return false;
+		}
+	}
 }
--- ValhallaVaultCyberAwareness/ValhallaVaultCyberAwareness.Client/ViewModels/SegmentUserScoreViewModel.cs
+++ ValhallaVaultCyberAwareness/ValhallaVaultCyberAwareness.Client/ViewModels/SegmentUserScoreViewModel.cs
@@ -12,6 +12,8 @@ namespace ValhallaVaultCyberAwareness.Client.ViewModels
         public List<SubCategoryScoreViewModel> SubCategoryScores { get; set; } = new();
         public int CorrectUserAnswers { get; set; }
         public int TotalQuestions { get; set; }
+        public int CompletedSubcategories { get; set; }
+        public int TotalSubCategories { get; set; }
         public double UserCompletionPercentage { get; set; }
         public bool UserHasCompletedSegment { get; set; }
         public int AvailableSubCategoryIndex { get; set; } = 0;
@@ -24,12 +26,22 @@ namespace ValhallaVaultCyberAwareness.Client.ViewModels
             SegmentDescription = segment.Description;
             SegmentSubCategories = segment.SubCategories;
             SubCategoryScores = segment.SubCategories.Select(s => new SubCategoryScoreViewModel(s)).ToList();
+            CompletedSubcategories = SubCategoryScores.Where(s => s.UserHasCompletedSubCategory).Count();
+            TotalSubCategories = SubCategoryScores.Count();
             //Calculate all correct useranswers in each subcategory and aggregate it to the segments total.
             CorrectUserAnswers = SubCategoryScores.Aggregate(0, (total, subCategoryScore) => total + subCategoryScore.CorrectUserAnswers);
             //Calculate all questions in each subcategory and aggregate it to the segments total.
             TotalQuestions = SubCategoryScores.Aggregate(0, (total, subCategoryScore) => total + subCategoryScore.TotalQuestions);
             //Calculate user success percentage in this segment.
-            UserCompletionPercentage = Math.Round(((double)CorrectUserAnswers / (double)TotalQuestions) * 100, 2);
+            if (TotalQuestions > 0)
+            {
+                UserCompletionPercentage = Math.Round(((double)CorrectUserAnswers / (double)TotalQuestions) * 100, 2);
+            }
+            else
+            {
+                UserCompletionPercentage = 0;
+                AvailableSubCategoryIndex = -1;
+            }
             //Calculate which subcategory the user is eligible to start from.
             for (int i = 0; i < SubCategoryScores.Count; i++)
             {
