f983112898707abb8249a3b09a2b50be5a83e94c
andrejakobsson7 <andre.jakobsson7@hotmail.com>
2024-03-16 14:46:15 +0100

    Setup useranswers cache

--- ValhallaVaultCyberAwareness/ValhallaVaultCyberAwareness/Controllers/UserAnswersController.cs
+++ ValhallaVaultCyberAwareness/ValhallaVaultCyberAwareness/Controllers/UserAnswersController.cs
@@ -1,31 +1,42 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OutputCaching;
 using ValhallaVaultCyberAwareness.Domain.Models;
 using ValhallaVaultCyberAwareness.Repositories.Interfaces;
 
 namespace ValhallaVaultCyberAwareness.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class UserAnswersController : ControllerBase
-    {
-        public IUserAnswersRepository _userAnswersRepo { get; set; }
+	[Route("api/[controller]")]
+	[ApiController]
+	public class UserAnswersController : ControllerBase
+	{
+		public IUserAnswersRepository _userAnswersRepo { get; set; }
+		private readonly IOutputCacheStore _outputCacheStore;
 
-        public UserAnswersController(IUserAnswersRepository userAnswerRepo)
-        {
-            _userAnswersRepo = userAnswerRepo;
-        }
+		public UserAnswersController(IUserAnswersRepository userAnswerRepo, IOutputCacheStore outputCacheStore)
+		{
+			_userAnswersRepo = userAnswerRepo;
+			_outputCacheStore = outputCacheStore;
+		}
 
-        [HttpPost]
-        public async Task<IActionResult> AddUserAnswersAsync(List<UserAnswers> newUserAnswers)
-        {
-            bool isSuccesfullyAdded = await _userAnswersRepo.AddUserAnswersAsync(newUserAnswers);
-            if (isSuccesfullyAdded)
-            {
-                return Ok();
-            }
-            return BadRequest();
-        }
+		[HttpPost]
+		[OutputCache(PolicyName = "ByIdCachePolicy")]
+		public async Task<IActionResult> AddUserAnswersAsync(List<UserAnswers> newUserAnswers, CancellationToken cancellationToken)
+		{
+			bool isSuccesfullyAdded = await _userAnswersRepo.AddUserAnswersAsync(newUserAnswers);
+			if (isSuccesfullyAdded)
+			{
+				string userId = newUserAnswers.First().UserId;
+				await RemoveFromUserCacheAsync(userId, cancellationToken);
+				return Ok();
+			}
+			return BadRequest();
+		}
+
+		private async Task RemoveFromUserCacheAsync(string userId, CancellationToken cancellationToken)
+		{
+			await _outputCacheStore.EvictByTagAsync(userId, cancellationToken);
+		}
 
 
-    }
+	}
 }
