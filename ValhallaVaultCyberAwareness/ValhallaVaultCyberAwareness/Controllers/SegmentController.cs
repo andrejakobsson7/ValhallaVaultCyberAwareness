46dd233bd3d0172b223dc913577969bf54162914
andrejakobsson7 <andre.jakobsson7@hotmail.com>
2024-03-26 22:04:46 +0100

    remove temporary segment-model from controller

--- ValhallaVaultCyberAwareness/ValhallaVaultCyberAwareness/Controllers/SegmentController.cs
+++ ValhallaVaultCyberAwareness/ValhallaVaultCyberAwareness/Controllers/SegmentController.cs
@@ -27,8 +27,7 @@ namespace ValhallaVaultCyberAwareness.Controllers
             var segmentScore = await _segmentRepo.GetSegmentWithUserScoresByUserIdAsync(segmentId, userId);
             if (segmentScore != null)
             {
-                SegmentApiModel apiSegmentScore = new SegmentApiModel(segmentScore);
-                var segmentScoresJson = JsonSerializer.Serialize(apiSegmentScore, _jsonSerializerOptions);
+                var segmentScoresJson = JsonSerializer.Serialize(segmentScore, _jsonSerializerOptions);
                 return Ok(segmentScoresJson);
             }
             return BadRequest();
@@ -59,44 +58,6 @@ namespace ValhallaVaultCyberAwareness.Controllers
                 return Ok(segmentToDelete);
             }
             return BadRequest();
-        }
-
-        //Temporary model that is returned from the API to enable that the client service can get access to navigation properties,
-        //since these are not serialized otherwise
-        public class SegmentApiModel
-        {
-            public int Id { get; set; }
-            public string Name { get; set; } = null!;
-            public string? Description { get; set; }
-            public int CategoryId { get; set; }
-            public List<SubCategoryModel> SubCategories { get; set; } = new();
-            public List<QuestionModel> Questions { get; set; } = new();
-            public List<AnswerModel> Answers { get; set; } = new();
-            public List<UserAnswers> UserAnswers { get; set; } = new();
-
-            public SegmentApiModel(SegmentModel segment)
-            {
-                Id = segment.Id;
-                Name = segment.Name;
-                Description = segment.Description;
-                CategoryId = segment.CategoryId;
-                foreach (var subCategory in segment.SubCategories)
-                {
-                    SubCategories.Add(subCategory);
-                    foreach (var question in subCategory.Questions)
-                    {
-                        Questions.Add(question);
-                        foreach (var answer in question.Answers)
-                        {
-                            Answers.Add(answer);
-                            foreach (var userAnswer in answer.UserAnswers)
-                            {
-                                UserAnswers.Add(userAnswer);
-                            }
-                        }
-                    }
-                }
-            }
         }
     }
 }
