8c12a84fa536bda50d5fed2abacec790b3c36a72
EmmaRhodin <143386528+EmmaRhodin@users.noreply.github.com>
2024-04-04 17:58:41 +0200

    Refactored CategoryScoreViewModel.cs & QuestionAnswerViewModel.cs

--- ValhallaVaultCyberAwareness/ValhallaVaultCyberAwareness.Client/ViewModels/QuestionAnswerViewModel.cs
+++ ValhallaVaultCyberAwareness/ValhallaVaultCyberAwareness.Client/ViewModels/QuestionAnswerViewModel.cs
@@ -19,19 +19,50 @@ namespace ValhallaVaultCyberAwareness.Client.ViewModels
         public bool IsAnswerSubmitted { get; set; }
         public QuestionAnswerViewModel(QuestionModel question)
         {
-            QuestionId = question.Id;
-            Question = question.Question;
-            Explanation = question.Explanation;
-            Answers = question.Answers;
+            QuestionId = GetQuestionId(question.Id);
+            Question = GetQuestion(question.Question);
+            Explanation = GetExplanation(question.Explanation);
+            Answers = GetAnswers(question.Answers);
             //Since the repository filters out invalid questions that doesn't have a correct answer registered, all questions that the service receives will have at least one correct answer.
+            CorrectAnswerId = GetCorrectAnswerId(question);
+            CorrectAnswer = GetCorrectAnswer(question);
+            /*
             AnswerModel correctAnswer = question.Answers.First(a => a.IsCorrect);
             CorrectAnswerId = correctAnswer.Id;
             CorrectAnswer = correctAnswer.Answer;
+            */
         }
 
         public QuestionAnswerViewModel()
         {
 
+        }
+
+        public int GetQuestionId(int id)
+        {
+            return id;
+        }
+        public string GetQuestion(string question)
+        {
+            return question;
+        }
+        public string GetExplanation(string explanation)
+        {
+            return explanation;
+        }
+        public List<AnswerModel> GetAnswers(List<AnswerModel> answers)
+        {
+            return answers.ToList();
+        }
+        public int GetCorrectAnswerId(QuestionModel question)
+        {
+            AnswerModel correctAnswer = question.Answers.First(a => a.IsCorrect);
+            return correctAnswer.Id;
+        }
+        public string? GetCorrectAnswer(QuestionModel question)
+        {
+            AnswerModel correctAnswer = question.Answers.First(a => a.IsCorrect);
+            return correctAnswer.Answer;
         }
     }
 }
