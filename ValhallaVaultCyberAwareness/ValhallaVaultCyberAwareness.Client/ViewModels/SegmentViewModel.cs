4ffb8d2bda061f60ba5c1f616773944e7910b288
andrejakobsson7 <andre.jakobsson7@hotmail.com>
2024-04-08 22:56:25 +0200

    add viewmodel for segment and validationattribute

---
+++ ValhallaVaultCyberAwareness/ValhallaVaultCyberAwareness.Client/ViewModels/SegmentViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using ValhallaVaultCyberAwareness.Client.Validators;
+using ValhallaVaultCyberAwareness.Domain.Models;
+
+namespace ValhallaVaultCyberAwareness.Client.ViewModels
+{
+    public class SegmentViewModel
+    {
+        [Required(ErrorMessage = "Segment name is required")]
+        public string Name { get; set; } = null!;
+
+        public string? Description { get; set; }
+
+        [NotZero(ErrorMessage = "Category is required")]
+        public int CategoryId { get; set; }
+
+        public SegmentViewModel()
+        {
+
+        }
+
+        public SegmentViewModel(SegmentModel segment)
+        {
+            Name = segment.Name;
+            Description = segment.Description;
+            CategoryId = segment.CategoryId;
+        }
+    }
+}
