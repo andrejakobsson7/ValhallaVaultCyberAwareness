c93e1e876daa43ec29ebff0793af6f96d8dc6f50
Dion <146066755+urie8@users.noreply.github.com>
2024-04-11 22:52:46 +0200

    Delete support/commands.js

--- support/commands.js
+++
@@ -1,6 +0,0 @@
-Cypress.Commands.add("login", (email, password) => {
-  cy.visit("/login.html");
-  cy.get("#email").type(email);
-  cy.get("#password").type(password);
-  cy.get('[type="submit"]').click();
-});
